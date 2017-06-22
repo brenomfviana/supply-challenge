7ffce8d3c352982b69f95d2249f8b97c0230e679
Patricia Cruz <patipc@hotmail.com>
2017-06-22 02:49:00 +0000

    Phase 2 almost complete

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# echo "Executando..." &&
+# go run "Phase $1/supply-challenge.go"
+cd "Phase_$1" &&
+go build &&
 echo "Executando..." &&
-go run "Phase $1/supply-challenge.go"
+start=`date +%s%N`/1000000 &&
+./"Phase_$1" &&
+end=`date +%s%N`/1000000 &&
+runtime=$(((end-start))) &&
+echo "Time: $runtime milliseconds" &&
+rm "Phase_$1"
